0fe141a29dff56ed390da418d71af91bc0183b4d
drxwat <drxwat@gmail.com>
2016-12-03 21:49:26 +0300

    little optimization

--- split_image_with_model.py
+++ split_image_with_model.py
@@ -5,6 +5,7 @@ from ocr.helper import save_image_batch
 from keras.models import load_model
 import ntpath
 import numpy as np
+import tqdm
 
 pyramid_scale, pyramid_min_width, pyramid_min_height = 0.8, 150, 150
 sl_w_step, sl_w_width, sl_w_height = 5, 30, 30
@@ -12,6 +13,7 @@ sl_w_step, sl_w_width, sl_w_height = 5, 30, 30
 ap = argparse.ArgumentParser(description='Detecting text blocks with sliding window algorithm.')
 ap.add_argument('image', help='Path to the image')
 ap.add_argument('model', type=str, help='Path to Keras model')
+ap.add_argument('output', type=str, help='Output directory (default: .)')
 # Pyramid arguments
 ap.add_argument('-ps', type=float, default=pyramid_scale, metavar='SCALE',
                 help='Pyramid scale rate (default: {})'.format(pyramid_scale))
@@ -29,8 +31,8 @@ ap.add_argument('-sh', type=int, default=sl_w_height, metavar='SW_HEIGHT',
 
 args = ap.parse_args()
 # Ugly assigning
-image_path, model_path, pyramid_scale, pyramid_min_width, pyramid_min_height, sl_w_step, sl_w_width, sl_w_height \
-    = args.image, args.model, args.ps, args.pw, args.ph, args.s, args.sw, args.sh
+image_path, model_path, directory_to_write, pyramid_scale, pyramid_min_width, pyramid_min_height, sl_w_step, sl_w_width, sl_w_height \
+    = args.image, args.model, args.output, args.ps, args.pw, args.ph, args.s, args.sw, args.sh
 
 # Preparing image and model
 model = load_model(model_path)
@@ -39,32 +41,32 @@ print('Model {} loaded.'.format(model_path))
 image = cv2.imread(image_path)
 
 # Processing image
-directory_to_write = '../results/'
 output_directory = '{}{}/'.format(directory_to_write, ntpath.basename(image_path))
 pyramid_number = 0
 batch_size = 128
 for pyramid_image in image_pyramid(image, scale=pyramid_scale, min_size=(pyramid_min_width, pyramid_min_height)):
 
     # Getting batches of images from image window algorithm
-    for window_batch in sliding_window_batch(pyramid_image, sl_w_step, (sl_w_width, sl_w_height), batch_size):
+    for window_batch in tqdm.tqdm(sliding_window_batch(pyramid_image, sl_w_step, (sl_w_width, sl_w_height), batch_size)):
         # Getting prediction from keras model
         predictions = model.predict(window_batch.reshape(batch_size, 3, 30, 30)/255)
 
-        # Getting filtered predictions
-        predictions_by_percent = {
-            '90': (predictions[:, 0] >= 0.9),
-            '80': (predictions[:, 0] < 0.9) & (predictions[:, 0] >= 0.8),
-            '70': (predictions[:, 0] < 0.8) & (predictions[:, 0] >= 0.7),
-            '60': (predictions[:, 0] < 0.7) & (predictions[:, 0] >= 0.6),
-            '50': (predictions[:, 0] < 0.6) & (predictions[:, 0] >= 0.5)
-        }
+        if bool(np.any(predictions[:, 0] >= 0.5)):
+            # Getting filtered predictions
+            predictions_by_percent = {
+                '90': (predictions[:, 0] >= 0.9),
+                '80': (predictions[:, 0] < 0.9) & (predictions[:, 0] >= 0.8),
+                '70': (predictions[:, 0] < 0.8) & (predictions[:, 0] >= 0.7),
+                '60': (predictions[:, 0] < 0.7) & (predictions[:, 0] >= 0.6),
+                '50': (predictions[:, 0] < 0.6) & (predictions[:, 0] >= 0.5)
+            }
 
-        # Saving relevant images
-        map(lambda predict_percent, predict_name:
-            save_image_batch(window_batch[predict_percent], '{}{}'.format(output_directory, predict_name))
-            if bool(np.any(predict_percent))
-            else None,
-            list(predictions_by_percent.values()), predictions_by_percent.keys())
+            # Saving relevant images
+            map(lambda predict_percent, predict_name:
+                save_image_batch(window_batch[predict_percent], '{}{}'.format(output_directory, predict_name))
+                if bool(np.any(predict_percent))
+                else None,
+                list(predictions_by_percent.values()), predictions_by_percent.keys())
 
     pyramid_number += 1
 
